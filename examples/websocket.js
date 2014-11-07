7ba9518e2f480da51c4964419bd429fb9555ebaa
marcoschwartz <marcolivier.schwartz@gmail.com>
2014-11-07 09:55:34 +0100

    updated dummy and added websocket example

--- examples/websocket.js
+++ examples/websocket.js
@@ -4,28 +4,15 @@ var app = express();
 var rest = require('./../index.js')(app);
 
 // Set ID & name
-var name = process.argv[3] || 'my_new_device';
-var id = process.argv[4] || '34f5eQ';
+var name = process.argv[3] || 'dummy';
+var id = process.argv[4] || 'p5dtgwwt';
 rest.set_id(id);
 rest.set_name(name);
-
-//rest.set_serial_port('/dev/slave');
-//rest.start_serial(9600);
 
 temperature = 24;
 humidity = 40;
 rest.variable('temperature',temperature);
 rest.variable('humidity',humidity);
-
-rest.function("led",function(command) {
-
-  // Get state from command
-  state = parseInt(command);
-
-  rest.digitalWrite(6,state);
-  
-  return 1;
-});
 
 // Define port & start server
 port = process.argv[2] || 3000;
@@ -34,5 +21,4 @@ var server = app.listen(port, function() {
 });
 
 // Connect via Websockets
-rest.connect_ws('http://localhost:3000', server.address().port);
-//rest.connect_ws('http://marcolivier-arest-cloud-test.jit.su/', server.address().port);
+rest.connect_ws('http://marcolivier-arest-cloud-test.jit.su/', server.address().port);
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
+// Requires
+var request = require("request");
+var WebSocket = require('ws');
+var serialport = require('serialport');
+var SerialPort = serialport.SerialPort;
+
 // aREST class
 var device = {
   id: '001',
   name: 'my_device',
+  serial_port: '',
+  serial_speed: '',
   variables: {},
   functions: {},
   digital_pins: [],
@@ -19,21 +27,109 @@ for (i=0; i < 6; i++) {
 
 // Exports
 module.exports = function (app) {
-    
-    // All
-    app.get('/', function(req, res){
-      
+
+    // Mode
+    app.get('/mode/:pin/:state', function(req, res){
+
+      console.log('Mode request');
+
       var answer = new Object();
+
       answer.id = device.id;
       answer.name  = device.name;
-      answer.variables = device.variables;
       answer.connected = true;
-      
+
+      if (req.params.state == 'i'){
+        answer.message = 'Pin D' + req.params.pin + ' set to input';  
+      }
+      if (req.params.state == 'o'){
+        answer.message = 'Pin D' + req.params.pin + ' set to output';
+      }
+
+      // Send answer
+      console.log(answer);
       res.json(answer);
+    });
+
+    // Digital write
+    app.get('/digital/:pin/:state', function(req, res){
+
+      console.log('Digital write request');
+
+      var answer = new Object();
+
+      answer.id = device.id;
+      answer.name  = device.name;
+      answer.connected = true;
+
+      answer.message = 'Pin D' + req.params.pin + ' set to ' + req.params.state;
+
+      // Set state
+      device.digital_pins[req.params.pin] = parseInt(req.params.state);
+
+      // Send answer
+      console.log(answer);
+      res.json(answer);
+    });
+
+    // Analog write
+    app.get('/analog/:pin/:state', function(req, res){
+
+      console.log('Analog write request');
+
+      var answer = new Object();
+
+      answer.id = device.id;
+      answer.name  = device.name;
+      answer.connected = true;
+
+      answer.message = 'Pin D' + req.params.pin + ' set to ' + req.params.state;
+
+      // Send answer
+      console.log(answer);
+      res.json(answer);
+    });
+
+    // Digital read
+    app.get('/digital/:pin', function(req, res){
+
+        console.log('Digital read request');
+
+          // Get value
+          value = device.digital_pins[req.params.pin];
+    
+          var answer = new Object();
+          answer.id = device.id;
+          answer.name  = device.name;
+          answer.connected = true;
+          answer.return_value = value;
+
+          console.log(answer);
+          res.json(answer);
+    });
+
+    // Analog read
+    app.get('/analog/:pin', function(req, res){
+
+          console.log('Analog read request');
+
+          // Get value
+          value = device.analog_pins[req.params.pin];
+    
+          var answer = new Object();
+          answer.id = device.id;
+          answer.name  = device.name;
+          answer.connected = true;
+          answer.return_value = value;
+
+          console.log(answer);
+          res.json(answer);
     });
 
     // Variables
     app.get('/:variable', function(req, res){
+
+      console.log('Variable or function request');
 
       var answer = new Object();
 
@@ -51,96 +147,80 @@ module.exports = function (app) {
         answer.return_value = value;
       }
 
+      console.log(answer);
       res.json(answer);
     });
 
-    // Mode
-    app.get('/mode/:pin/:state', function(req, res){
+    // All
+    app.get('/', function(req, res){
 
+      console.log('Root request');
+      
       var answer = new Object();
-
       answer.id = device.id;
       answer.name  = device.name;
+      answer.variables = device.variables;
       answer.connected = true;
-
-      if (req.params.state == 'i'){
-        answer.message = 'Pin D' + req.params.pin + ' set to input';  
-      }
-      if (req.params.state == 'o'){
-        answer.message = 'Pin D' + req.params.pin + ' set to output';
-      }
-
-      // Send answer
+      
+      console.log(answer);
       res.json(answer);
-    });
-
-    // Digital write
-    app.get('/digital/:pin/:state', function(req, res){
-
-      var answer = new Object();
-
-      answer.id = device.id;
-      answer.name  = device.name;
-      answer.connected = true;
-
-      answer.message = 'Pin D' + req.params.pin + ' set to ' + req.params.state;
-
-      // Set state
-      device.digital_pins[req.params.pin] = parseInt(req.params.state);
-
-      // Send answer
-      res.json(answer);
-    });
-
-    // Analog write
-    app.get('/analog/:pin/:state', function(req, res){
-
-      var answer = new Object();
-
-      answer.id = device.id;
-      answer.name  = device.name;
-      answer.connected = true;
-
-      answer.message = 'Pin D' + req.params.pin + ' set to ' + req.params.state;
-
-      // Send answer
-      res.json(answer);
-    });
-
-    // Digital read
-    app.get('/digital/:pin', function(req, res){
-
-          // Get value
-          value = device.digital_pins[req.params.pin];
-    
-          var answer = new Object();
-          answer.id = device.id;
-          answer.name  = device.name;
-          answer.connected = true;
-          answer.return_value = value;
-          res.json(answer);
-    });
-
-    // Analog read
-    app.get('/analog/:pin', function(req, res){
-
-          // Get value
-          value = device.analog_pins[req.params.pin];
-    
-          var answer = new Object();
-          answer.id = device.id;
-          answer.name  = device.name;
-          answer.connected = true;
-          answer.return_value = value;
-          res.json(answer);
     });
 
   return {
+    start_serial: function(serial_speed) {
+      device.serial_speed = serial_speed;
+
+      device.serialPort = new SerialPort(device.serial_port, {
+        baudrate: serial_speed,
+        parser: serialport.parsers.readline('\n')
+      }, function(error){
+        if (error){
+          console.log("Error with request");
+        }
+      });
+
+      // Handle Serial data
+      device.serialPort.on("open", function () {
+        console.log('Serial port opened');
+        device.serialPort.on('close', function(data) {
+          console.log('Serial port closed');
+        });
+        device.serialPort.on('data', function(data) {
+          console.log('data received: ' + data);
+          setTimeout(function(){
+            device.serialPort.close();
+          },500);
+      });
+      device.serialPort.write(Buffer('Works' + '\r'));
+    });  
+
+    },
+    connect_ws: function(remote_server, port) {
+
+      var ws = new WebSocket(remote_server);
+
+      ws.on('open', function() {
+        console.log('Opened WebSocket connection');
+
+        ws.on('message', function(message) {
+          console.log('Received command: %s', message);
+          request('http://localhost:' + port + '/' + message, function(error, response, body) {
+            console.log('Returned command: %s', body);
+            ws.send(body);
+          });
+        });
+
+      });
+    
+    },
     set_id: function(new_id) {
       device.id = new_id;
     },
     set_name: function(new_name) {
       device.name = new_name;
+    },
+    set_serial_port: function(serial_port) {
+      device.serial_port = serial_port;
     },
     variable: function(variable_name,variable_value){
       device.variables[variable_name] = variable_value;  
